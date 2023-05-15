6206519b26ad6a835050ab1a46c936bd6f4748f6
Murielle <a.muriellesarah@gmail.com>
2023-05-15 18:49:17 +0100

    first project step

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App.jsx';
+
+// ReactDom.render(<App/> , document.getElementById('root')); //THIS LINE INDICATE THAT
+// ALL THE CONTENT OF THIS FILE SHOULD BE RENDERED IN THE ROOT ID OF THE HTML FILE
