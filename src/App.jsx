3df4c774f628c42b6037f2d12635f4e0f90288a8
SMurielle <a.muriellesarah@gmail.com>
2023-05-22 14:31:41 +0100

    added app.jsx, index.css and index.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React from 'react'
 import './index.css';
 import Header from './components/header/Header'
 
-function App() {
-return (
-<>
-<Header/>
 
-</>
-);
+const App = () => {
+  return (
+    <>
+        <Header>
+    </>
+
+  );
 }
-export default App;
+
+export default App
+
+
+
+// import React from 'react';
+// import './index.css';
+// import Header from './components/header/Header'
+
+// function App() {
+// return (
+// <>
+// <Header/>
+// </>
+// );
+// }
+// export default App;
